7306578ecbc608aff1e08dc1b0f62048f8a75b0b
Nikhil Shinde <snikhil9282@gmail.com>
2025-03-25 14:12:42 +0530

    Tenant Successfully Set

--- src/main/java/com/institute/config/MultiTenantDataSource.java
+++ src/main/java/com/institute/config/MultiTenantDataSource.java
@@ -27,46 +27,19 @@
 //    }git
 //}
 //
-package com.institute.config;
-
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-
-import javax.sql.DataSource;
-
-public class MultiTenantDataSource extends AbstractRoutingDataSource {
-
-    private final MultiTenantDataSourceConfig dataSourceConfig;
-
-    public MultiTenantDataSource(MultiTenantDataSourceConfig dataSourceConfig) {
-        this.dataSourceConfig = dataSourceConfig;
-    }
-
-    @Override
-    protected Object determineCurrentLookupKey() {
-        return TenantContext.getCurrentTenant();
-    }
-
-    @Override
-    protected DataSource determineTargetDataSource() {
-        String tenant = (String) determineCurrentLookupKey();
-        if (tenant == null) {
-            throw new IllegalStateException("No tenant found!");
-        }
-        return dataSourceConfig.resolveDataSource(tenant);
-    }
-}
-//
 //package com.institute.config;
 //
-//
 //import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+//
 //import javax.sql.DataSource;
-//import org.springframework.beans.factory.annotation.Autowired;
 //
 //public class MultiTenantDataSource extends AbstractRoutingDataSource {
 //
-//    @Autowired
-//    private MultiTenantDataSourceConfig dataSourceConfig;
+//    private final MultiTenantDataSourceConfig dataSourceConfig;
+//
+//    public MultiTenantDataSource(MultiTenantDataSourceConfig dataSourceConfig) {
+//        this.dataSourceConfig = dataSourceConfig;
+//    }
 //
 //    @Override
 //    protected Object determineCurrentLookupKey() {
@@ -82,6 +55,33 @@ public class MultiTenantDataSource extends AbstractRoutingDataSource {
 //        return dataSourceConfig.resolveDataSource(tenant);
 //    }
 //}
+//
+package com.institute.config;
+
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MultiTenantDataSource extends AbstractRoutingDataSource {
+
+    @Autowired
+    private MultiTenantDataSourceConfig dataSourceConfig;
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return TenantContext.getCurrentTenant();
+    }
+
+    @Override
+    protected DataSource determineTargetDataSource() {
+        String tenant = (String) determineCurrentLookupKey();
+        if (tenant == null) {
+            throw new IllegalStateException("No tenant found!");
+        }
+        return dataSourceConfig.resolveDataSource(tenant);
+    }
+}
 
 
 
